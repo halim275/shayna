b371527da70fc56d31042efa6ce003a54312e838
halim275 <halim.devlop@gmail.com>
2022-04-22 00:35:59 +0700

    HomeView

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="home">
+  <div class="product">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <HeaderShayna />
-    <HeroShayna />
-    <WomanShayna />
-    <InstaShayna />
-    <LogoShayna />
+
     <FooterShayna />
   </div>
 </template>
@@ -14,10 +11,7 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import HeaderShayna from "@/components/HeaderShayna.vue";
-import HeroShayna from "@/components/HeroShayna.vue";
-import WomanShayna from "@/components/WomanShayna.vue";
-import InstaShayna from "@/components/InstaShayna.vue";
-import LogoShayna from "@/components/LogoShayna.vue";
+
 import FooterShayna from "@/components/FooterShayna.vue";
 
 export default {
@@ -25,10 +19,7 @@ export default {
   components: {
     // HelloWorld
     HeaderShayna,
-    HeroShayna,
-    WomanShayna,
-    InstaShayna,
-    LogoShayna,
+
     FooterShayna,
   },
 };
